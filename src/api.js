f4a66906a09754330189aa002c2060bdf896b2b6
s-c-meyer <meyer.samuelc@gmail.com>
2023-11-21 16:52:25 -0600

    Added offline functionality

--- src/api.js
+++ src/api.js
@@ -48,6 +48,12 @@ export const getEvents = async () => {
     return mockData;
   } 
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events?JSON.parse(events):[];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -56,6 +62,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem('lastEvents', JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
